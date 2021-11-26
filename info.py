ff095f2abf62d561ddb8691e63354ca0a77ec713
Noob-Hacker71 <80197475+Noob-Hacker71@users.noreply.github.com>
2021-11-26 11:12:34 +0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,48 @@
+import os
+os.system("clear")
+G="\033[1;32m"
+B="\033[1;34m"
+Y="\033[1;33m"
+P="\033[1;35m"
+C="\033[1;36m"
+design="""
+{G}╔═╗ ╔╗     ╔╗      {C}╔╗ ╔╗     ╔╗       {Y}╔═══╗╔╗
+{G}║║╚╗║║     ║║      {C}║║ ║║     ║║       {Y}║╔═╗╠╝║
+{G}║╔╗╚╝╠══╦══╣╚═╗    {C}║╚═╝╠══╦══╣║╔╦══╦╩╣{Y}  ╔╝╠╗║
+{G}║║╚╗║║╔╗║╔╗║╔╗╠{P} ══ {C}╣╔═╗║╔╗║╔═╣╚╝╣║═╣╔╝{Y}  ║╔╝║║
+{G}║║ ║║║╚╝║╚╝║╚╝╠{P} ══ {C}╣║ ║║╔╗║╚═╣╔╗╣║═╣║ {Y}  ║║╔╝╚╗
+{G}╚╝ ╚═╩══╩══╩══╝   {C} ╚╝ ╚╩╝╚╩══╩╝╚╩══╩╝ {Y}  ╚╝╚══╝
+""".format(P=P,C=C,G=G,Y=Y)
+
+
+logo=("""{B}
+*************************************************************
+{G}\tRemember >>  •••Allah Always Watching You•••
+{B}*************************************************************
+  {C}     ____We Work To Sarve You a Clean Cyber Space____
+
+    {Y}      ____Well Come To Noob-Hacker's World____
+
+{C}	    ___We Hack to Protect Bangaladedh___
+
+{Y}	      ___ WebDo Not Hack To Impress___
+
+  {C}		 ___We  Are  Anonymouse___
+
+{Y}		  ___We Hack to Express___
+
+{C}		   ___We Do Not Forget___
+
+{Y}		    ___We Don't Forgive___
+
+{C}		     ___We Are Legion___
+
+{B}*************************************************************
+{ascii}
+{B}*************************************************************
+{G}Author   >>  Tahsan-Nayem [Noob-Hacker71]
+{Y}Facebook >>  https://facebook.com/Noob.Hacker71
+{C}Github   >>  https://githun.com/Noob-Hacker71
+{B}*************************************************************
+""".format(ascii=design,B=B,C=C,G=G,Y=Y))
+print(logo)
