2ed74290723b904c3fdfb58939868b898a8537ab
Noob-Hacker71 <80197475+Noob-Hacker71@users.noreply.github.com>
2021-11-28 20:25:10 +0400

    Update info.py

--- info.py
+++ info.py
@@ -16,34 +16,25 @@ design="""
 
 
 logo=("""{B}
-*************************************************************
+*********************************************************
 {G}\tRemember >>  •••Allah Always Watching You•••
-{B}*************************************************************
+{B}*********************************************************
   {C}     ____We Work To Sarve You a Clean Cyber Space____
-
     {Y}      ____Well Come To Noob-Hacker's World____
-
 {C}	    ___We Hack to Protect Bangaladedh___
-
 {Y}	      ___ WebDo Not Hack To Impress___
-
   {C}		 ___We  Are  Anonymouse___
-
 {Y}		  ___We Hack to Express___
-
 {C}		   ___We Do Not Forget___
-
 {Y}		    ___We Don't Forgive___
-
 {C}		     ___We Are Legion___
-
-{B}*************************************************************
+{B}*********************************************************
 {ascii}
-{B}*************************************************************
+{B}*********************************************************
 {G}Author   >>  Tahsan-Nayem [Noob-Hacker71]
 {Y}Facebook >>  https://facebook.com/Noob.Hacker71
 {C}Github   >>  https://githun.com/Noob-Hacker71
-{B}*************************************************************
+{B}*********************************************************
 """.format(ascii=design,B=B,C=C,G=G,Y=Y))
 
 User="Noob-Hacker71"
