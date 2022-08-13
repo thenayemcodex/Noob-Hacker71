fb8a211ffacf8ffe09c6e0bc895215a19e7aa64a
Noob-Community <80197475+Noob-Hacker71@users.noreply.github.com>
2022-08-13 17:56:59 +0400

    Update update.py

--- update.py
+++ update.py
@@ -3,3 +3,7 @@ we are going to add veries thingk in this app.
 so keep support us and stay with us
 thank you
 """
+
+terms = """
+this app is only for Educational perpose . authority is not responsible for your any illigle action. thank you
+"""
