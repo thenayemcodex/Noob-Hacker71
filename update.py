d27eead46600b365e9a73a1b7e113ab4175a7d71
Noob-Community <80197475+Noob-Hacker71@users.noreply.github.com>
2022-08-17 16:54:22 +0400

    Update update.py

--- update.py
+++ update.py
@@ -7,3 +7,5 @@ thank you
 terms = """
 this app is only for Educational perpose . authority is not responsible for your any illigle action. thank you
 """
+
+Adverties = "https://github.com/Noob-Hacker71/Noob-Hacker71/PicsArt_11-07-01.46.59.png"
