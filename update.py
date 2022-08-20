581411cb4d3a0298ea35715925195d54ea4f064c
Noob-Community <80197475+Noob-Hacker71@users.noreply.github.com>
2022-08-20 21:02:03 +0400

    Update update.py

--- update.py
+++ update.py
@@ -9,3 +9,6 @@ this app is only for Educational perpose . authority is not responsible for your
 """
 
 Adverties = "https://github.com/Noob-Hacker71/Noob-Hacker71/PicsArt_11-07-01.46.59.png"
+
+
+keyword = ['fuck', 'sexy', 'paku', 'cudi', 'heda', 'sauya', 'gay', 'lesbo', 'lesbian', 'fucking', 'porn', 'pornhub', 'Brazzers','xxx','xxnx','xvideo', 'noughty','madarchod', 'bainchod', 'toxic'] 
